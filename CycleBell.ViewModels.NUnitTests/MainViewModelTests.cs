3c9196717024064ae106c86558aae19dd05a8235
p1eXu5 <v1adp1exu5@gmail.com>
2018-08-27 19:45:50 +0300

    MainViewModelTests     TestInitializer()

--- CycleBell.ViewModels.NUnitTests/MainViewModelTests.cs
+++ CycleBell.ViewModels.NUnitTests/MainViewModelTests.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace CycleBell.ViewModels.NUnitTests
 {
+    [TestFixture]
     public class MainViewModelTests
     {
+        [SetUp]
+        public void TestInitializer()
+        {}
+
+
     }
 }
