202374a8c9699424caf16f0fe59ecd8e430018d3
p1eXu5 <v1adp1exu5@gmail.com>
2018-09-13 20:30:29 +0300

    add DataTemplate SelectTemplate

---
+++ Code/Main Project/CycleBell/DataTemplateSelectors/TimePointVmDataTemplateSelector.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using CycleBell.ViewModels;
+
+namespace CycleBell.DataTemplateSelectors
+{
+    public class TimePointVmDataTemplateSelector : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate (object item, DependencyObject container)
+        {
+            if (container is FrameworkElement element) {
+                if (item is BeginTimePointViewModel) {
+
+                    return element.FindResource ("datatempl_BeginTimePointViewModel") as DataTemplate;
+                }
+                if (item is EndTimePointViewModel) {
+
+                    return element.FindResource ("datatempl_EndTimePointViewModel") as DataTemplate;
+                }
+                if (item is TimePointViewModel) {
+
+                    return element.FindResource ("datatempl_TimePointViewModel") as DataTemplate;
+                }
+
+            }
+
+            return null;
+        }
+    }
+}
