00c0f044f64d6d1325d0e2bb7a7ba7275abfea1f
p1eXu5 <v1adp1exu5@gmail.com>
2018-09-19 11:53:47 +0300

    -add BorderColor attahed property

---
+++ Code/Main Project/CycleBell/Views/AttachedPropertyFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace CycleBell.Views
+{
+    public static class AttachedPropertyFactory
+    {
+
+        public static readonly DependencyProperty BorderColorProperty = DependencyProperty.RegisterAttached("BorderColor", typeof(Color), typeof(AttachedPropertyFactory)
+                                                                                                            , new FrameworkPropertyMetadata(Colors.DimGray, FrameworkPropertyMetadataOptions.Inherits));
+
+        public static void SetBorderColor(DependencyObject d, Color value)
+        {
+            d.SetValue(AttachedPropertyFactory.BorderColorProperty, value);
+        }
+
+        public static Color GetBorderColor(DependencyObject d)
+        {
+            return (Color) d.GetValue(AttachedPropertyFactory.BorderColorProperty);
+        }
+    }
+}
