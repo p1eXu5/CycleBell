762396df8bf9902054159c01bfe690858c3dd28e
p1eXu5 <v1adp1exu5@gmail.com>
2018-09-25 11:17:09 +0300

    -fix ZIndex/Opacity bug, restruct Experiments

--- Experiments/WpfElementsDesign/Properties/AssemblyInfo.cs
+++ Experiments/WpfElementsDesign/Properties/AssemblyInfo.cs
@@ -10,9 +10,9 @@ using System.Windows;
 [assembly: AssemblyTitle("WpfApp1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WpfApp1")]
-[assembly: AssemblyCopyright("Copyright © HP Inc. 2018")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
