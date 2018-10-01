b814de50324fbc32de0d78dbd25dd302b9107310
p1eXu5 <v1adp1exu5@gmail.com>
2018-10-01 15:59:56 +0300

    -add tasks

--- Code/Main Project/CycleBell/App.xaml.cs
+++ Code/Main Project/CycleBell/App.xaml.cs
@@ -17,6 +17,18 @@ namespace CycleBell
     {
         protected override void OnStartup(StartupEventArgs e)
         {
+            // TODO tasks:
+            //      1.  Ring button
+            //      2.  Correct IsEnable triggers in data templates
+            //      3.  Vertical scroll control template (restyle)
+            //      4.  PresetsComboBox (layers or template?), save, save as
+            //      5.  Export/Import presets
+            //      6.  LoopNumber ComboBox
+            //      7.  Help
+            //      8.  License
+            //      9.  Deploy
+            //      10. DrugAndDrop?
+
             base.OnStartup (e);
 
             TimePoint.DefaultTimePointNameFunc = point => $"Time point {point.Id}";
