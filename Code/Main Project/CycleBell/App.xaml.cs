379fa1ea4748ed7942cbcc3a40d78e4e40133d3e
p1eXu5 <v1adp1exu5@gmail.com>
2018-10-03 22:59:17 +0300

    -update tasks

--- Code/Main Project/CycleBell/App.xaml.cs
+++ Code/Main Project/CycleBell/App.xaml.cs
@@ -18,8 +18,7 @@ namespace CycleBell
         protected override void OnStartup(StartupEventArgs e)
         {
             // TODO tasks:
-            //      2.  Correct IsEnable triggers in data templates
-            //      4.  PresetsComboBox (layers or template?), save, save as
+            //      4.  PresetsComboBox (layers or template?), save, save as => Fake TextBox
             //      5.  Export/Import presets
             //      6.  LoopNumber ComboBox
             //      7.  Help
