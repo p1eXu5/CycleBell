1c862866643a1b1bc2d4cb145efcbafa7e3b47de
p1eXu5 <v1adp1exu5@gmail.com>
2018-10-31 14:43:45 +0300

    -add FakeMainWindow class, STA

--- Tests/CycleBell.NUnitTests/Integrational Tests/MainViewModelIntegrationalTests.cs
+++ Tests/CycleBell.NUnitTests/Integrational Tests/MainViewModelIntegrationalTests.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using CycleBell.Base;
 using CycleBell.ViewModels;
 using CycleBellLibrary.Context;
@@ -12,12 +9,18 @@ using CycleBellLibrary.Repository;
 using CycleBellLibrary.Timer;
 using Moq;
 using NUnit.Framework;
-using NUnit.Framework.Internal;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Media;
+
 // ReSharper disable AccessToModifiedClosure
 
 namespace CycleBell.NUnitTests.Integrational_Tests
 {
-    [TestFixture]
+    [TestFixture, RequiresThread(ApartmentState.STA)]
     public class MainViewModelIntegrationalTests
     {
         [Test]
@@ -61,16 +64,29 @@ namespace CycleBell.NUnitTests.Integrational_Tests
 
         private MainViewModel GetMainViewModel(Preset[] presets = null)
         {
+            if (Thread.CurrentThread.GetApartmentState() != ApartmentState.STA) {
+                throw new ThreadStateException("The current threads apartment state is not STA");
+            }
+
             var obscol = new ObservableCollection<Preset>();
             var rObscol = new ReadOnlyObservableCollection<Preset>(obscol);
 
             _mockTimerManager = _mockCycleBellManager.As<ITimerManager>();
             _mockPresetCollectionManager = _mockCycleBellManager.As<IPresetCollectionManager>();
 
-            var pcm = new PresetCollectionManager();
-            var cbm = new CycleBellManager (null, pcm, _mockTimerManager.Object);
 
-            var mainViewModel = new MainViewModel (_mockDialogRegistrator.Object, cbm);
+            var pcm = new PresetCollectionManager();
+            ICycleBellManager cbm = null; 
+
+            try {
+                cbm = new CycleBellManager("some_file_name", pcm, _mockTimerManager.Object);
+            }
+            catch (ArgumentNullException) { }
+
+            var mainViewModel = cbm != null ? new MainViewModel (_mockDialogRegistrator.Object, cbm) : throw new ArgumentNullException(nameof(cbm), @"Instance of ICycleBellManager is null");
+
+            Window wnd = new FakeMainWindow();
+            wnd.DataContext = mainViewModel;
 
             return mainViewModel;
         }
@@ -84,6 +100,70 @@ namespace CycleBell.NUnitTests.Integrational_Tests
             preset.AddTimePoint(TimePoint.GetAbsoluteTimePoint());
 
             return preset;
+        }
+
+        private class FakeMainWindow : Window
+        {
+
+            public FakeMainWindow()
+            {
+                var dock = new DockPanel();
+                dock.LastChildFill = true;
+
+                var menu = new Menu();
+                menu.Height= Double.NaN;
+                menu.Background = Brushes.White;
+                DockPanel.SetDock(menu, Dock.Top);
+
+                var statusBar = new StatusBar();
+                statusBar.Background = Brushes.Transparent;
+                statusBar.BorderThickness = new Thickness(0);
+                statusBar.FontSize = 10;
+                DockPanel.SetDock(statusBar, Dock.Bottom);
+
+                var textBlock = new TextBlock();
+                var statusBarBinding = new Binding();
+                statusBarBinding.Path = new PropertyPath("StatusBarText");
+                statusBarBinding.Mode = BindingMode.OneWay;
+                statusBarBinding.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+                textBlock.SetBinding(TextBlock.TextProperty, statusBarBinding);
+
+                var statusBarItem = new StatusBarItem();
+                statusBarItem.Content = textBlock;
+                statusBar.Items.Add(statusBarItem);
+
+                var presetsGrid = new Grid();
+                presetsGrid.VerticalAlignment = VerticalAlignment.Stretch;
+                TextElement.SetFontSize(presetsGrid, 14.0);
+
+                var presets = new ComboBox();
+
+                var collBinding = new Binding();
+                collBinding.Path = new PropertyPath("PresetViewModelCollection");
+                collBinding.Mode = BindingMode.OneWay;
+
+                presets.SetBinding(ItemsControl.ItemsSourceProperty, collBinding);
+
+                var selBinding = new Binding();
+                selBinding.Path = new PropertyPath("SelectedPreset");
+                selBinding.Mode = BindingMode.TwoWay;
+                selBinding.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+
+                presets.SetBinding(Selector.SelectedItemProperty, selBinding);
+
+                var textBinding = new Binding();
+                textBinding.Path = new PropertyPath("SelectedPresetName");
+                textBinding.Mode = BindingMode.TwoWay;
+                textBinding.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+
+                presets.SetBinding(ComboBox.TextProperty, textBinding);
+
+                presetsGrid.Children.Add(presets);
+
+                dock.Children.Add(presetsGrid);
+
+                Content = dock;
+            }
         }
 
         #endregion
