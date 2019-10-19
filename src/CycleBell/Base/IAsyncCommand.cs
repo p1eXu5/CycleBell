3a0d08a083ccc1aa6d861049aac9aa83e331d8da
First Second <v1adp1exu5@gmail.com>
2019-10-19 17:05:41 +0300

    chore: add MvvmAsyncCommand

---
+++ src/CycleBell/Base/IAsyncCommand.cs
@@ -0,0 +1,11 @@
+﻿
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CycleBell.Base
+{
+    public interface IAsyncCommand : ICommand
+    {
+        Task ExecuteAsync ( object obj );
+    }
+}
---
+++ src/CycleBell/Base/TaskExtensions.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Threading.Tasks;
+
+namespace CycleBell.Base
+{
+    public static class TaskExtensions
+    {
+        public static async void FireAndForgetSafeAsync ( this Task task, IErrorHandler handler = null )
+        {
+            try {
+                await task;
+            }
+            catch( Exception ex ) {
+
+                handler?.HandleError( ex );
+            }
+        }
+    }
+}
