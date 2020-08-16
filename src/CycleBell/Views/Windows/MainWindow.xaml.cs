e3ffe46e8eef44cc1870a2fd569d77b54ce4f3a6
First Second <v1adp1exu5@gmail.com>
2020-08-16 13:28:30 +0300

    [ chore ] - add ActiveTimePointProperty dp to the MainWindow

--- src/CycleBell/Views/Windows/MainWindow.xaml.cs
+++ src/CycleBell/Views/Windows/MainWindow.xaml.cs
@@ -1,12 +1,53 @@
 ﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shell;
+using CycleBell.ViewModels.TimePointViewModels;
 
-namespace CycleBell
+namespace CycleBell.Views.Windows
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static readonly DependencyProperty ActiveTimePointProperty =
+            DependencyProperty.RegisterAttached(
+                "ActiveTimePoint",
+                typeof( TimePointViewModelBase ),
+                typeof( MainWindow ),
+                new FrameworkPropertyMetadata( 
+                    null, 
+                    FrameworkPropertyMetadataOptions.SubPropertiesDoNotAffectRender,
+                    new PropertyChangedCallback( OnActiveTimePointChanged ))
+                );
+
+        private static void OnActiveTimePointChanged( DependencyObject d, DependencyPropertyChangedEventArgs e )
+        {
+            if ( d is Visual child ) 
+            {
+                DependencyObject parent = VisualTreeHelper.GetParent( child );
+                MainWindow mv = parent as MainWindow;
+
+                while ( parent != null && mv == null ) {
+                    parent = VisualTreeHelper.GetParent( parent );
+                    mv = parent as MainWindow;
+                }
+
+                if ( mv != null ) 
+                {
+                    TaskbarItemInfo tb = mv.m_TaskbarItemInfo;
+                    tb.ProgressState = TaskbarItemProgressState.Error;
+                    tb.ProgressValue = 1.0;
+                }
+            }
+            
+        }
+
+        public static void SetActiveTimePoint( DependencyObject d, TimePointViewModelBase value )
+        {
+            d.SetValue( ActiveTimePointProperty, value );
+        }
+
         public MainWindow()
         {
             InitializeComponent();
